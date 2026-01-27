3e4dcad2ce17c0fdcda72415555009697e639a5b
alphegasolutions <john.akinyele@alphegasolutions.com>
2026-01-27 09:02:54 -0500

    upgraded tailwind from v3 to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
-
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite'; // Import the new Vite plugin
 
-// https://astro.build/config
 export default defineConfig({
     site: process.env.VITE_WEBSITE_URL || 'https://oasishealthservices.com',
     integrations: [
         react(),
-        tailwind({
-            applyBaseStyles: false, // We'll import our own base styles
-        }),
-        sitemap(),
+        sitemap()
     ],
+    vite: {
+        plugins: [tailwindcss()], // Add Tailwind as a Vite plugin here
+    },
 });
